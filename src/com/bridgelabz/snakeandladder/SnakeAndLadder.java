e6ca3468205744ef5e1add2757a263c6cf8a7787
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-08-01 18:16:15 +0530

    Add : Initial Position of a player

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -3,5 +3,7 @@ package com.bridgelabz.snakeandladder;
 public class SnakeAndLadder {
 	public static void main(String args[]) {
 		System.out.println("Welcome to Snake and Ladder..!");
+		int playerPosition = 0;
+		System.out.println("Initially the player at position : "+playerPosition);
 	}
 }
