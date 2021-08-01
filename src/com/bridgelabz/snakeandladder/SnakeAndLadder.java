1bc6e36ee3935099e1685ca2ca2fec55549a35dc
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-08-01 18:40:12 +0530

    Add : Rolling the die to get a number

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -1,9 +1,12 @@
 package com.bridgelabz.snakeandladder;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 	public static void main(String args[]) {
 		System.out.println("Welcome to Snake and Ladder..!");
-		int playerPosition = 0;
-		System.out.println("Initially the player at position : "+playerPosition);
+		Random random = new Random();
+		int randomNumber = 1 + random.nextInt(6);
+		System.out.println("Number shown by die is : " + randomNumber);
 	}
 }
