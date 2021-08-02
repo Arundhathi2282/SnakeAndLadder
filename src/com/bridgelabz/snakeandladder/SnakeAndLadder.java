39d573c5330cdb6c5cf18eabaa201c781a2f3886
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-08-02 07:31:46 +0530

    Add : Checking for exact winning Position

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class SnakeAndLadder {
 	/**
 	 * Checking for an option like NoPlay, Ladder and Snake 
-	 * Moving the player position according to the option
-	 * Repeating the loop till player reaches to 100th position
+	 * Moving the player position according to the option 
+	 * Repeating the loop till player reaches to exact winning position
 	 */
 	public void CheckingOfAnOption() {
 		int playerPosition = 0;
@@ -16,17 +16,25 @@ public class SnakeAndLadder {
 			int randomNumber = 1 + random.nextInt(6);
 			switch (randomCheck) {
 			case 1:
+				System.out.println("The player lands at the base of the ladder");
 				playerPosition += randomNumber;
+				System.out.println("The position of player is : " + playerPosition);
 				break;
 			case 2:
+				System.out.println("The player lands on a top of a snake");
 				playerPosition -= randomNumber;
+				System.out.println("The position of player is : " + playerPosition);
 				break;
 			default:
+				System.out.println("The player has choosen to noplay");
 				playerPosition = playerPosition;
+				System.out.println("The position of player is : " + playerPosition);
 			}
 			if (playerPosition < 0) {
 				playerPosition = 0;
-			}else {
+			} else if (playerPosition > 100) {
+				playerPosition -= randomNumber;
+			} else {
 				if (playerPosition == 100) {
 					System.out.println("The player has won the game by reaching winning position");
 					break;
